f7587380b27ab9a047a858b4f54019f41f4f3b20
Santiago Sanchez Giraldo <77745043+InsertName-x23@users.noreply.github.com>
2024-05-21 13:20:26 -0500

    Delete app directory

--- app/src/main/java/com/example/appnativa_finance/fragment1.kt
+++
@@ -1,6 +0,0 @@
-package com.example.appnativa_finance
-
-import androidx.fragment.app.Fragment
-
-class fragment1: Fragment(R.layout.fragment1) {
-}
--- app/src/main/java/com/example/appnativa_finance/fragment2.kt
+++
@@ -1,6 +0,0 @@
-package com.example.appnativa_finance
-
-import androidx.fragment.app.Fragment
-
-class fragment2: Fragment(R.layout.fragment2) {
-}
