ef6563acb2233b27f2f48722d18090229b30a953
InsertName-x23 <ssg5670@gmail.com>
2024-05-21 16:16:25 -0500

    Error fragment Fixed

---
+++ Project/app/src/main/java/com/example/entregaappmoviles/FragmentAddFolder.kt
@@ -0,0 +1,7 @@
+package com.example.entregaappmoviles
+
+import androidx.fragment.app.Fragment
+
+class FragmentAddFolder: Fragment(R.layout.fragment_add_folder) {
+
+}
--- Project/app/src/main/java/com/example/entregaappmoviles/MainActivity.kt
+++ Project/app/src/main/java/com/example/entregaappmoviles/MainActivity.kt
@@ -1,32 +1,36 @@
 package com.example.entregaappmoviles
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import androidx.activity.ComponentActivity
 import androidx.appcompat.app.AppCompatActivity
-import com.example.entregaappmoviles.databinding.ActivityMainBinding
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.content.ContextCompat
-import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentTransaction
-import com.example.entregaappmoviles.ui.theme.EntregaAppMovilesTheme
+import com.example.entregaappmoviles.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
 
-
+    lateinit var binding: MainActivityBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if(savedInstanceState == null) {
+            replaceFragment(FragmentHome())
+        }
+
+        binding.addFolderBtn.setOnClickListener() {
+            replaceFragment(FragmentAddFolder())
+        }
+
+        binding.homeButton.setOnClickListener() {
+            replaceFragment(FragmentHome())
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.commit()
     }
 }
 
